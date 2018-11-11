bd9c0b93738b4f2b46a60d140f015afe8251c5ae
p-well <p.a.kadantsev@gmail.com>
2018-11-11 17:21:15 +0300

    Typos correction

--- wordscounter/wsgi.py
+++ wordscounter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wordcounter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wordscounter.settings")
 
 application = get_wsgi_application()
